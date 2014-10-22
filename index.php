5c14b04a5a651e88b92a750196d1874b35235296
Merili <meriliasu@gmail.com>
2014-10-22 19:00:59 +0300

    Don't really know how to do the last part :)
    Don't really know how to do the last part :)

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
+<?php 
+	if (isset($_POST["submit"])) {
+	$username = $_POST['username'];
+	$password = $_POST['password'];
+	$message = "Vormis esines vigu!";} 
+	else { 
+	$message = "Palun logi sisse!";}
+ ?>
 <!DOCTYPE HTML>
 <html>
 <head>
@@ -5,6 +13,18 @@
 	<title>2. periood 4. praktikum</title>
 </head>
 <body>
+	<h2><?php echo $message;?></h2>	
+	<form name="loginform" action="index.php" method="post">
+		<span>Kasutajanimi:</span>
+			<input type="text" name="username" value="<?php if (isset($_POST["submit"])){
+			$username = $_POST['username'];}?>">
+		<br>
+		<span>Parool:</span>
+			<input type="password" class="parool" name="password">
+		<br>
+			<input type="submit" name="submit" value="Saada">
+	</form>
+	<pre><?php print_r($_POST);?></pre>
 </body>
 </html>
 	
